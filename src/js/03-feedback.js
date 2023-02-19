eaa77ed4b854c2900cd8ada788f46406043fe587
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-02-19 13:36:12 +0200

    tes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,66 @@
 import throttle from 'lodash.throttle';
+
 const formEl = document.querySelector('.feedback-form');
 const emailEl = formEl.querySelector('input');
 const messageEl = formEl.querySelector('textarea');
 
-// Зберігаємо дані у локальне сховище з затримкою
-const saveFormState = throttle(() => {
-  const formState = {
-    email: emailEl.value,
-    message: messageEl.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
-}, 500);
+const KEY = 'feedback-form-state';
+const formData = {};
+formEl.addEventListener('submit', formSubmit);
+formEl.addEventListener('input', throttle(onInput, 500));
+formInput();
 
-// Відстежуємо подію input на полях форми
-formEl.addEventListener('input', saveFormState);
-
-// Заповнюємо поля форми при завантаженні сторінки
-window.addEventListener('load', () => {
-  const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (formState) {
-    emailEl.value = formState.email;
-    messageEl.value = formState.message;
+function formSubmit(e) {
+  e.preventDefault();
+  if (textareaEl.value === '' || inputEl.value === '') {
+    return;
   }
-});
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY);
+}
 
-// Очищуємо форму та сховище при сабміті
-formEl.addEventListener('submit', event => {
-  event.preventDefault();
-  localStorage.removeItem('feedback-form-state');
-  emailEl.value = '';
-  messageEl.value = '';
-});
+function onInput(e) {
+  formData.email = inputEl.value.trim();
+  formData.message = textareaEl.value.trim();
+
+  return localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function formInput() {
+  const data = JSON.parse(localStorage.getItem(KEY));
+  if (data) {
+    (inputEl.value = data.email || ''),
+      (textareaEl.value = data.message || '');
+  }
+  return data;
+}
+
+// // Зберігаємо дані у локальне сховище з затримкою
+// const saveFormState = throttle(() => {
+//   const formState = {
+//     email: emailEl.value,
+//     message: messageEl.value,
+//   };
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+// }, 500);
+
+// // Відстежуємо подію input на полях форми
+// formEl.addEventListener('input', saveFormState);
+
+// // Заповнюємо поля форми при завантаженні сторінки
+// window.addEventListener('load', () => {
+//   const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+//   if (formState) {
+//     emailEl.value = formState.email;
+//     messageEl.value = formState.message;
+//   }
+// });
+
+// // Очищуємо форму та сховище при сабміті
+// formEl.addEventListener('submit', event => {
+//   event.preventDefault();
+//   localStorage.removeItem('feedback-form-state');
+//   emailEl.value = '';
+//   messageEl.value = '';
+// });
+
