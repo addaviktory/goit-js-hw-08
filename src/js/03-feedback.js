ee2c459189b7af72a3ba048d60592fd4ca9b9160
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-02-19 13:26:35 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ const formEl = document.querySelector('.feedback-form');
 const emailEl = formEl.querySelector('input');
 const messageEl = formEl.querySelector('textarea');
 
+// Зберігаємо дані у локальне сховище з затримкою
 const saveFormState = throttle(() => {
   const formState = {
     email: emailEl.value,
@@ -11,10 +12,22 @@ const saveFormState = throttle(() => {
   localStorage.setItem('feedback-form-state', JSON.stringify(formState));
 }, 500);
 
+// Відстежуємо подію input на полях форми
 formEl.addEventListener('input', saveFormState);
+
+// Заповнюємо поля форми при завантаженні сторінки
+window.addEventListener('load', () => {
+  const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formState) {
+    emailEl.value = formState.email;
+    messageEl.value = formState.message;
+  }
+});
+
+// Очищуємо форму та сховище при сабміті
 formEl.addEventListener('submit', event => {
-  event.preventDefault()
-  localStorage.removeItem('feedback-form-state')
-  emailEl.value = ''
-  messageEl.value = ''
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailEl.value = '';
+  messageEl.value = '';
 });
