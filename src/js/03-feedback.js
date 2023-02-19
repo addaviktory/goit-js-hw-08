bfa1e06d9641fd05be26d6cf93881c1237700b92
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-02-19 13:41:36 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,7 +12,7 @@ formInput();
 
 function formSubmit(e) {
   e.preventDefault();
-  if (textareaEl.value === '' || inputEl.value === '') {
+  if (messageEl.value === '' || emailEl.value === '') {
     return;
   }
   e.currentTarget.reset();
@@ -20,8 +20,8 @@ function formSubmit(e) {
 }
 
 function onInput(e) {
-  formData.email = inputEl.value.trim();
-  formData.message = textareaEl.value.trim();
+  formData.email = emailEl.value.trim();
+  formData.message = messageEl.value.trim();
 
   return localStorage.setItem(KEY, JSON.stringify(formData));
 }
@@ -29,38 +29,15 @@ function onInput(e) {
 function formInput() {
   const data = JSON.parse(localStorage.getItem(KEY));
   if (data) {
-    (inputEl.value = data.email || ''),
-      (textareaEl.value = data.message || '');
+    (emailEl.value = data.email || ''),
+      (messageEl.value = data.message || '');
   }
   return data;
 }
-
-// // Зберігаємо дані у локальне сховище з затримкою
-// const saveFormState = throttle(() => {
-//   const formState = {
-//     email: emailEl.value,
-//     message: messageEl.value,
-//   };
-//   localStorage.setItem('feedback-form-state', JSON.stringify(formState));
-// }, 500);
-
-// // Відстежуємо подію input на полях форми
-// formEl.addEventListener('input', saveFormState);
-
-// // Заповнюємо поля форми при завантаженні сторінки
-// window.addEventListener('load', () => {
-//   const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
-//   if (formState) {
-//     emailEl.value = formState.email;
-//     messageEl.value = formState.message;
-//   }
-// });
-
-// // Очищуємо форму та сховище при сабміті
-// formEl.addEventListener('submit', event => {
-//   event.preventDefault();
-//   localStorage.removeItem('feedback-form-state');
-//   emailEl.value = '';
-//   messageEl.value = '';
-// });
-
+window.addEventListener('load', () => {
+      const formState = JSON.parse(localStorage.getItem(KEY));
+      if (formState) {
+        emailEl.value = formState.email;
+        messageEl.value = formState.message;
+      }
+    });
